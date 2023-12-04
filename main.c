8300441bb8104b791fe69d287df52824e0f9d2a7
Ninevv <aineb.emi7@gmail.com>
2023-12-04 10:51:08 +0100

    Question 2

--- main.c
+++ main.c
@@ -2,24 +2,47 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
-#define BUFSIZE 2
-int main() {
-    char outbuf[] = "/.enseash\nWelcome to ENSEA Tiny Shell.\nType 'exit' to quit.\nenseash %"; //
+#define BUFSIZE 128
+
+int main(int argc, char **argv __attribute__((unused))) {
+    char outbuf[] = "Welcome to ENSEA Tiny Shell.\nType 'exit' to quit.\nenseash% "; //
     char inbuf[BUFSIZE];
-    int status = 0;
-    write(STDOUT_FILENO, outbuf, strlen(outbuf));// STDOUT_FILENO
+    char res[BUFSIZE];
+    int status ;
+    ssize_t inlength;
+    pid_t childPID;
+    pid_t childtermination;
+    char* terminatedstatus;
+    const int witpidoption = 0;
+
+    write(STDOUT_FILENO, outbuf, strlen(outbuf));
     //read(STDIN_FILENO, inbuf, BUFSIZE);
     //execlp("ls","ls",(char * )NULL);
-    while(1){
-        read(STDIN_FILENO, inbuf[:-2], BUFSIZE);//noooooon
 
-        if(status = execlp(inbuf,inbuf,(char * )NULL)!=0){
-            perror("execlp");
-            sprintf(outbuf, "status:%d\n", status);
+    while(1){
+        inlength = read(STDIN_FILENO, inbuf, BUFSIZE);
+        inbuf[inlength-1] = '\0';
+
+        /*char debug1[BUFSIZE];
+        sprintf(debug1,"length: %d\n", inlength);
+        write(STDOUT_FILENO, debug1, strlen(debug1));*/
+
+        childPID = fork();
+        if(childPID == 0) { // child process
+            if ((status = execlp(inbuf, inbuf, (char *) NULL)) != 0) {
+                perror("execlp");
+                sprintf(outbuf, "status:%d\n", status);
+                write(STDOUT_FILENO, outbuf, strlen(outbuf));
+            };
+        }
+        else { //parent process
+            while((childtermination = waitpid(childPID,terminatedstatus,witpidoption)) ==0){};
+            sprintf(outbuf, "enseash%% ");
             write(STDOUT_FILENO, outbuf, strlen(outbuf));
-        };
-        exit(0);
+        }
     }
     return 0;
 }
